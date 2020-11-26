40f1be8222ca85bc9b6beaa55fca5ed9bd158a1b
Fatih Tunahan Dumanli <fatihdumanli@live.com>
2020-11-26 10:17:15 +0300

    deleted some files

--- publish.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git config --global user.email "$USER_EMAIL"
-git config --global user.name "$USER_NAME"
-
-git checkout -b "$1"
-git merge release/ts
-git commit -m "$CIRCLE_BRANCH build#$CIRCLE_BUILD_NUM"
-git push --force --set-upstream origin $1
